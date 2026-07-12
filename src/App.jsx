a696a90f33a1bfc2bfe91a4862f26517e9f96016
Your Name <esthereluehike19@gmail.com>
2026-07-12 15:01:43 -0700

    Build search results page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import EventDetails from "./pages/eventdetails";
 import Explore from "./pages/Explore";
+import SearchResults from "./pages/SearchResults";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/event/:id" element={<EventDetails />} />
       <Route path="/explore" element={<Explore />} />
+      <Route path="/search" element={<SearchResults />} />
     </Routes>
   );
 }
---
+++ src/pages/SearchResults.jsx
@@ -0,0 +1,34 @@
+import "./SearchResults.css";
+
+import Navbar from "../components/Navbar/Navbar";
+import SearchBar from "../components/SearchBar/SearchBar";
+import EventGrid from "../components/EventGrid/EventGrid";
+import Footer from "../components/Footer/Footer";
+
+function SearchResults() {
+  return (
+    <>
+      <Navbar />
+
+      <main className="search-results-page">
+        <div className="search-results-header">
+          <h1>Search Results</h1>
+
+          <p>
+            Showing results for <strong>"Music"</strong>
+          </p>
+        </div>
+
+        <div className="search-bar-wrapper">
+          <SearchBar />
+        </div>
+
+        <EventGrid />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default SearchResults;
