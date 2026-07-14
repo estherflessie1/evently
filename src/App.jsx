c5b87ddbe451b8cc8352b1d67b3da580666ec896
Your Name <esthereluehike19@gmail.com>
2026-07-14 01:51:30 -0700

    Make navbar responsive for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import EventDetails from "./pages/eventdetails";
 import Explore from "./pages/Explore";
 import SearchResults from "./pages/SearchResults";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 import logo from "../../assets/logo.png";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">
@@ -11,21 +15,35 @@ function Navbar() {
         </Link>
       </div>
 
-      <ul className="nav-links">
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
         </li>
 
         <li>
-          <Link to="/explore">Explore</Link>
+          <Link to="/explore" onClick={() => setMenuOpen(false)}>
+            Explore
+          </Link>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
+        </li>
+
+        <li>
+          <button className="mobile-sign-btn">Sign In</button>
         </li>
       </ul>
 
       <button className="sign-in-btn">Sign In</button>
+
+      <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </button>
     </nav>
   );
 }
